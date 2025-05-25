321e1713f937091c282d7eaa37a78ff573b0f5b2
Miguel Jara <95490111+Miguel-A-Jara@users.noreply.github.com>
2025-05-25 00:36:56 -0300

    feat: implement settings screen

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,5 +1,32 @@
-import { Text } from 'react-native'
+import { Pressable } from 'react-native'
+
+import Container from '@/components/layout/container'
+import { Label } from '@/components/ui/label'
+import { Separator } from '@/components/ui/separator'
+import { Switch } from '@/components/ui/switch'
+import { useColorScheme } from '@/hooks/useColorScheme'
+
+const THEME_ID = 'dark-mode'
 
 export default function Settings() {
-  return <Text>Settings</Text>
+  const { isDarkColorScheme, toggleColorScheme } = useColorScheme()
+
+  return (
+    <Container>
+      <Pressable
+        className='flex-row items-center justify-between active:bg-foreground/5 p-4 rounded-t-xl'
+        onPress={toggleColorScheme}
+      >
+        <Label nativeID={THEME_ID} onPress={toggleColorScheme}>
+          Tema Actual: {isDarkColorScheme ? 'Oscuro' : 'Claro'}
+        </Label>
+        <Switch
+          checked={isDarkColorScheme}
+          onCheckedChange={toggleColorScheme}
+          nativeID={THEME_ID}
+        />
+      </Pressable>
+      <Separator />
+    </Container>
+  )
 }
